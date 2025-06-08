f98c3d1d65b7f0bf9e52c780f6b4359fbfe3a29b
Alden Walker <akwalke@ccr-lajolla.org>
2025-06-07 19:47:20 -0700

    maybe working

--- akwalke_hook_length.py
+++ akwalke_hook_length.py
@@ -1,4 +1,4 @@
-def from_lists(L):
+def tableau_from_lists(L):
     """
     Map entry j in list i to answer[(i,j)]
     """
@@ -33,27 +33,113 @@ def maximal_position_with_standard_subtableau(T):
             return (i,j)
     return None
 
-def slide_pos(T, pos):
+def modified_forward_slide(T, pos):
     """
     While T^{<=pos} is not standard, swap pos and min of T[(i+1,j)], T[(i,j+1)]
     Assumes that T^{<=pos} is not standard initially
-    return the path
+    return the final tableau and the path
     """
     TT = T.copy()
     i,j = pos
     path = [(i,j)]
     while True:
-        print(TT)
+        # print(TT)
         nbrs    = [(i+1,j), (i,j+1)]
         options = [(TT[(k,l)], (k,l)) for (k,l) in nbrs if (k,l) in TT and TT[(k,l)] < TT[(i,j)]]
-        print((i,j), nbrs, options)
+        # print((i,j), nbrs, options)
         if len(options) == 0:
             break
         _, (ni,nj) = min(options)
         path.append((ni,nj))
         TT[(i,j)], TT[(ni,nj)], (i,j) = TT[(ni,nj)], TT[(i,j)], (ni,nj)
-        
-    return path
+    return TT, path
+
+def funky_J(J, path):
+    """
+    Given the current J_{k-1} and next slide path,
+    return the next J_k
+    """
+    JJ = J.copy()
+    (i0,j0), *_, (i1,j1) = path
+    HJ = [(h,j) for (h,j) in JJ if j == j0]
+    for (h,j) in HJ:
+        if i0 <= h and h < i1:
+            JJ[(h,j)] = J[(h+1,j)]-1
+        elif h == i1:
+            JJ[(h,j)] = j1 - j0
+    return JJ
+
+def NPS(T):
+    """
+    Do the bijection tableau T -> pair (P, J) with 
+    P standard, J hook, and all of the same shape
+    """
+    P_history = [T.copy()]
+    J_history = [{(i,j):0 for (i,j) in T}]
+    pos_history = []
+    while (pos := maximal_position_with_standard_subtableau(P_history[-1])) is not None:
+        pos_history.append(pos)
+        next_P, path = modified_forward_slide(P_history[-1], pos)
+        next_J = funky_J(J_history[-1], path)
+        P_history.append(next_P)
+        J_history.append(next_J)
+    return P_history, J_history, pos_history
+
+def n_ascii(s):
+    """
+    Return the number of ascii characters in the string s
+    """
+    return len([x for x in s if x.isascii()])
+
+def display_tableau(T, special_pos=None):
+    """
+    Return a string which displays a tableau
+    """
+    import unicodedata
+    n_rows    = max(i for (i,j) in T) + 1
+    n_cols    = max(j for (i,j) in T) + 1
+    flair     = lambda pos,v: (f'{v}\u0332' if pos == special_pos else str(v))
+    Tstrs     = {x:flair(x,v) for (x,v) in T.items()}
+    col_width = max(n_ascii(s) for s in Tstrs.values())
+    ans = [
+        ' '.join(f"{Tstrs.get((i,j), ''):>{col_width}}" for j in range(n_cols))
+        for i in range(n_rows)
+    ]
+    return ans
+
+def display_tableau_list(L, special_poses=None):
+    """
+    Return a string which displays a list of tableaus horizontally next to each other
+    """
+    if special_poses is not None:
+        blocks = [display_tableau(T, sp) for T,sp in zip(L, special_poses)]
+    else:
+        blocks = [display_tableau(T) for T in L]
+    height = max(len(b) for b in blocks)
+    for b in blocks:
+        if len(b) < height:
+            pad = n_ascii(b[0])+' '
+            b.extend((height-len(b))*[pad])
+    ans = []
+    for i in range(height):
+        line = '   '.join(b[i] for b in blocks)
+        ans.append(line)
+    ans = '\n'.join(ans) + '\n'
+    return ans
+
+T = tableau_from_lists(
+    [
+        [6, 2],
+        [4, 3],
+        [5, 1]
+    ]
+)
+
+PH, JH, poses = NPS(T)
+
+print(display_tableau_list(PH, poses+[None]))
+print()
+print(display_tableau_list(JH))
 
 
 
